0fe7e34985947e11f9f00067618fa4c76599fe11
jorgemartiinez19 <jorgemartiinez19@gmail.com>
2020-04-29 20:14:34 +0200

    menu responsive, multiples arreglos para varios formatos de pantalla en varias de las secciones

--- js/main.js
+++ js/main.js
@@ -1,12 +1,34 @@
-const menuBtn = document.querySelector('.nav__burger');
-const navLinks = document.querySelector('.nav__links');
-var menuOpen = false;
-menuBtn.addEventListener('click', function () {
-  if (!menuOpen) {
-    navLinks.classList.add('show-menu');
-    menuOpen = true;
-  } else {
-    navLinks.classList.remove('show-menu');
-    menuOpen = false;
-  }
+$(document).ready(function () {
+  console.log('ready!');
+  var menuBtn = $('.nav__burger');
+  var navItems = $('.nav__item');
+  var menuOpen = false;
+  menuBtn.click(function () {
+    if (!menuOpen) {
+      navItems.addClass('u-flexify');
+      menuOpen = true;
+    } else {
+      navItems.removeClass('u-flexify');
+      menuOpen = false;
+    }
+  });
+
+  $(window)
+    .scroll(function () {
+      var windscroll = $(window).scrollTop();
+      if (windscroll >= 100) {
+        $('nav').addClass('u-shadow');
+        $('.wrapper section').each(function (i) {
+          if ($(this).position().top <= windscroll - 100) {
+            $('nav a.active').removeClass('active');
+            $('nav a').eq(i).addClass('active');
+          }
+        });
+      } else {
+        $('nav').removeClass('u-shadow');
+        $('nav a.active').removeClass('active');
+        $('nav a:first').addClass('active');
+      }
+    })
+    .scroll();
 });
