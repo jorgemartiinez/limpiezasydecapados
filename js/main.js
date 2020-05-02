1b950190b32659c0bfe599c366fc84c52498e511
jorgemartiinez19 <jorgemartiinez19@gmail.com>
2020-05-02 17:36:47 +0200

    añadido smooth scroll con jquery, cambiado el color al seleccionar texto y cambiado color barra lateral en chrome

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
   console.log('ready!');
+
+  // * MENÚ MOVIL
   var menuBtn = $('.nav__burger');
   var navItems = $('.nav__item');
   var menuOpen = false;
@@ -13,6 +15,29 @@ $(document).ready(function () {
     }
   });
 
+  // ---------------------
+  // * animar scroll en los links que empiezen con #
+  $(document).on('click', 'a[href^="#"]', function (e) {
+    // target element id
+    var id = $(this).attr('href');
+
+    // target element
+    var $id = $(id);
+    if ($id.length === 0) {
+      return;
+    }
+
+    // prevent standard hash navigation (avoid blinking in IE)
+    e.preventDefault();
+
+    // top position relative to the document
+    var pos = $id.offset().top - 50;
+
+    // animated top scrolling
+    $('body, html').animate({ scrollTop: pos });
+  });
+  // -----------------
+  // * añadir clase para elevar el nav al empezar a hacer scroll
   $(window)
     .scroll(function () {
       var windscroll = $(window).scrollTop();
