5d1326e1f9fc77b884a45e1588463c5e26e710ad
jorgemartiinez19 <jorgemartiinez19@gmail.com>
2020-05-06 14:08:21 +0200

    intentando crear menu lateral

--- js/main.js
+++ js/main.js
@@ -3,16 +3,17 @@ $(document).ready(function () {
 
   // * MENÚ MOVIL
   var menuBtn = $('.nav__burger');
-  var navItems = $('.nav__links');
-  var menuOpen = false;
+  var navItems = $('.nav__links-wrapper');
+  var navCross = $('.nav__cross');
+
   menuBtn.click(function () {
-    if (!menuOpen) {
-      navItems.addClass('u-flexify');
-      menuOpen = true;
-    } else {
-      navItems.removeClass('u-flexify');
-      menuOpen = false;
-    }
+    navItems.addClass('show-menu');
+    navItems.removeClass('hide-menu');
+  });
+
+  navCross.click(function () {
+    navItems.removeClass('show-menu');
+    navItems.addClass('hide-menu');
   });
 
   // ---------------------
@@ -43,17 +44,31 @@ $(document).ready(function () {
       var windscroll = $(window).scrollTop();
       if (windscroll >= 100) {
         $('nav').addClass('u-shadow');
-        $('.wrapper section').each(function (i) {
-          if ($(this).position().top <= windscroll - 100) {
-            $('nav a.active').removeClass('active');
-            $('nav a').eq(i).addClass('active');
-          }
-        });
+        // $('.wrapper section').each(function (i) {
+        //   if ($(this).position().top <= windscroll - 100) {
+        //     $('nav a.active').removeClass('active');
+        //     $('nav a').eq(i).addClass('active');
+        //   }
+        // });
       } else {
         $('nav').removeClass('u-shadow');
-        $('nav a.active').removeClass('active');
-        $('nav a:first').addClass('active');
       }
     })
     .scroll();
+
+  //aaa
+  // // Optimalisation: Store the references outside the event handler:
+  var $window = $(window);
+
+  function checkWidth() {
+    var windowsize = $window.width();
+    if (windowsize > 899) {
+      navItems.removeClass('hide-menu');
+      navItems.removeClass('show-menu');
+    }
+  }
+  // Execute on load
+  checkWidth();
+  // Bind event listener
+  $(window).resize(checkWidth);
 });
